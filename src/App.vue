47e7668b389ffca12b27453373d9b69930eae3d4
alex <1414818093@qq.com>
2019-06-28 15:41:56 +0800

    引入头部组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div id="wrap">
+		<!-- 等同于 <the-header></the-header> -->
+    <TheHeader/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+	// 在使用 import 引入模块时，我们使用了 @ 符号，该别名指向的是 src 目录，
+	// 你可以从 webpack 配置文件 build/webpack.base.conf.js 找到其定义
+	import TheHeader from '@/components/layouts/TheHeader'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'App',
+		components: {
+			// 进行局部注册
+			TheHeader
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+	$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+	$fa-font-path: "~font-awesome/fonts/";
+	@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+	@import "~font-awesome/scss/font-awesome";
 </style>
