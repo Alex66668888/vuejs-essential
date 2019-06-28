7c7bb448143000504b5933201b591df6fbd5f27b
alex <1414818093@qq.com>
2019-06-28 16:46:29 +0800

    切换导航条列表并做兼容性调整

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,7 +1,16 @@
 <template>
 	<div class="navbar navbar-default topnav">
 		<div class="container">
+			
 			<div class="navbar-header">
+				
+				<button type="button" class="navbar-toggle" @click="toggleNav">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>				
+				
 				<a href="/" class="navbar-brand">
 					<!--  使用 Mustache 语法 （双大括号）进行数据绑定 -->
 					<span class="title">{{ logo.title }}</span>
@@ -9,7 +18,18 @@
 					<!-- src 和 alt 都是 HTML 特性，不能使用 Mustache 语法，而要使用 v-bind 指令，:src 是 v-bind:src 的缩写 -->
 					<img :src="logo.src" :alt="logo.title" />
 				</a>
+				
 			</div>
+			
+			<!-- 'collapse' 和 'navbar-collapse' 是默认要应用的类名，只有当 showCollapsedNav 返回 true 时才添加 'in' 这个类名 -->
+			<div id="top-navbar-collapse" :class="['collapse', 'navbar-collapse', { in: showCollapsedNav }]">
+				<ul class="nav navbar-nav">
+				  <li v-for="(item, index) in navList" :class="{ active: index === activeNavIndex }">
+					<a href="#" @click="changeNavIndex(index)">{{ item }}</a>
+				  </li>
+				</ul>
+			</div>
+			
 		</div>
 	</div>
 </template>
@@ -27,7 +47,11 @@
 					// src 的值是用反引号（`）标识的模板字符串，我们只需将变量放在 ${} 的大括号之中
 					src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
 					title: 'VuejsCaff'					
-				}
+				},
+				navList: ['社区', '头条', '问答', '教程'],
+				activeNavIndex: 0,
+				// 添加一个是否显示折叠导航的开关 showCollapsedNav
+				showCollapsedNav: false
 			}
 		},
 		
@@ -36,12 +60,25 @@
 		beforeCreate() {
 			// 在当前实例上添加一个 uploadsUrl 属性
 			this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
-		}
+		},
 		
 		// ES6 允许我们简写对象内的方法，因此下面两种写法是等价的：
 		// beforeCreate: function beforeCreate() {
 		//   this.uploadsUrl = 'https://vuejscaffcdn.phphub.org/uploads/'
 		// }
+		
+		methods: {
+			
+			changeNavIndex(index) {
+				this.activeNavIndex = index
+			},
+			
+			toggleNav() {
+				this.showCollapsedNav = !this.showCollapsedNav
+			}
+			
+		}
+		
 	}
 </script>
 
@@ -49,5 +86,8 @@
 <style scoped>
 	.title {
 		display: none;
+	}
+	.navbar-default .navbar-nav > .active > a {
+		background: rgba(0,0,0,.03);
 	}
 </style>
