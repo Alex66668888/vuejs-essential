0e5fdbfacda310ad0f63c6657fde449fde8c874f
alex <1414818093@qq.com>
2019-07-01 16:57:10 +0800

    4.5完善登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,19 @@ const routes = [
 		// 对应的视图组件
 		// 使用下面的方法指定组件，可以实现路由懒加载，即当路由被访问时才加载对应的组件
 		component: () => import('@/views/auth/Register')
-	}
+	},
+  // 首页路由配置
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  // 其他未配置的路由都跳转到首页
+  {
+    path: '*',
+    // 重定向
+    redirect: '/'
+  }	
 ]
 
 const router = new Router({
@@ -21,6 +33,19 @@ const router = new Router({
 	// 值 'history' 可利用 History API 来完成页面跳转且无须重新加载
 	mode: 'history',
 	routes
+})
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+	// 获取仓库里的登录信息
+  const auth = router.app.$options.store.state.auth
+
+  if (auth && to.path.indexOf('/auth/') !== -1) {
+		// 如果当前用户已登录，且目标路由包含 /auth/ ，就跳转到首页
+    next('/')
+  } else {
+    next()
+  }
 })
 
 export default router
